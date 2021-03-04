39ebaa46c4a24cfcbfd7d54938f4973cbf851626
Cody Alvarez <cody_alvarez19@outlook.com>
2021-03-04 01:41:59 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -35,7 +35,7 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
   shipping_manifest = {"muskets" => "2"}
-  hash["muskets"] = "2"
+  # hash["muskets"] = "2"
 
 
 
